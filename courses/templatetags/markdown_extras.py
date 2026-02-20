308fac4a023a0fce1f8c5b533a30afc2673bc6bf
Jost Prevc <jost@predictleads.com>
2026-02-20 21:26:47 +0100

    Add markdown_extras.py

---
+++ courses/templatetags/markdown_extras.py
@@ -0,0 +1,17 @@
+import markdown
+from django import template
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+
+@register.filter
+def markdown_to_html(value: str) -> str:
+    """Render markdown text to HTML. Use with {{ content|markdown_to_html }} in templates."""
+    if not value:
+        return ""
+    html = markdown.markdown(
+        value,
+        extensions=["extra", "nl2br", "sane_lists"],
+    )
+    return mark_safe(html)
